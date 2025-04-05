eaf352bb2e578867590bb5826397100295a16810
ibelyaev <ibelyaev@f2a0b9fc2c08>
2025-04-05 03:41:57 +0300

    update informatiom Notice job

--- logger.py
+++ logger.py
@@ -1,17 +1,40 @@
 import logging
 
+# ANSI-цвета
+LOG_COLORS = {
+    'DEBUG': '\033[94m',    # синий
+    'INFO': '\033[92m',     # зелёный
+    'WARNING': '\033[93m',  # жёлтый
+    'ERROR': '\033[91m',    # красный
+    'CRITICAL': '\033[95m', # фиолетовый
+    'RESET': '\033[0m',     # сброс
+}
+
+class ColorFormatter(logging.Formatter):
+    def format(self, record):
+        color = LOG_COLORS.get(record.levelname, LOG_COLORS['RESET'])
+        reset = LOG_COLORS['RESET']
+        record.levelname = f"{color}{record.levelname}{reset}"
+        return super().format(record)
+
 def setup_logger():
     logger = logging.getLogger("hestia_migrator")
     logger.setLevel(logging.DEBUG)
 
-    # Консольный вывод
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
 
-    formatter = logging.Formatter("[%(asctime)s] %(levelname)s - %(message)s")
+    formatter = ColorFormatter("[%(asctime)s] %(levelname)s - %(message)s")
     ch.setFormatter(formatter)
 
-    logger.addHandler(ch)
+    if not logger.handlers:
+        logger.addHandler(ch)
+
     return logger
 
 
+# log.debug("Отладочная инфа")
+# log.info("Инфо сообщение")
+# log.warning("Внимание")
+# log.error("Ошибка")
+# log.critical("Критическая ошибка")
