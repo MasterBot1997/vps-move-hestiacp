19282b30c57ae461beb50ded090823556110249c
ibelyaev <git@horrorbot.ru>
2025-04-07 01:28:43 +0300

    update

--- logger.py
+++ logger.py
@@ -2,20 +2,22 @@ import logging
 
 # ANSI-цвета
 LOG_COLORS = {
-    'DEBUG': '\033[94m',    # синий
-    'INFO': '\033[92m',     # зелёный
-    'WARNING': '\033[93m',  # жёлтый
-    'ERROR': '\033[91m',    # красный
-    'CRITICAL': '\033[95m', # фиолетовый
-    'RESET': '\033[0m',     # сброс
+    "DEBUG": "\033[94m",  # синий
+    "INFO": "\033[92m",  # зелёный
+    "WARNING": "\033[93m",  # жёлтый
+    "ERROR": "\033[91m",  # красный
+    "CRITICAL": "\033[95m",  # фиолетовый
+    "RESET": "\033[0m",  # сброс
 }
+
 
 class ColorFormatter(logging.Formatter):
     def format(self, record):
-        color = LOG_COLORS.get(record.levelname, LOG_COLORS['RESET'])
-        reset = LOG_COLORS['RESET']
+        color = LOG_COLORS.get(record.levelname, LOG_COLORS["RESET"])
+        reset = LOG_COLORS["RESET"]
         record.levelname = f"{color}{record.levelname}{reset}"
         return super().format(record)
+
 
 def setup_logger():
     logger = logging.getLogger("hestia_migrator")
--- main.py
+++ main.py
@@ -2,39 +2,54 @@ import logger
 import argparse
 
 parser = argparse.ArgumentParser(
-    prog='vps-moove-hestia',
-    description='Auto vps move',
-    epilog='Example of calling a script: <pattern>',
-    add_help=False)
+    prog="vps-moove-hestia",
+    description="Auto vps move",
+    epilog="Example of calling a script: <pattern>",
+    add_help=False,
+)
 
 
-parser.add_argument('-u', 
-                    '--user', 
-                    help='Логин хоста', 
-                    required=True)
+parser.add_argument("-u", "--user", help="Логин хоста", required=True)
 
-parser.add_argument('-h', 
-                    '--host', 
-                    help='host откуда синкаем', required=True)
-parser.add_argument('-w', '--web-root', help='Путь расположения файлов на исходном сервере', required=True)
-parser.add_argument('-p', metavar='PORT', help='Задать порт ssh в случае если на исходном сервере кастомный порт')
-parser.add_argument('--pas', metavar='PASSWORD', help='Если нельзя прокинуть ключ, то можно для автоматизаци задать пароль')
-parser.add_argument('--fix-permission', help='Исправление прав')
-parser.add_argument('--php', help='Задать требуемую версию php при разворачивании автоматически')
-parser.add_argument('--log-file', help='Кастомный путь до лог файла, удобнее когда много сайтов переносится')
-parser.add_argument('--help', action='help')
+parser.add_argument("-h", "--host", help="host откуда синкаем", required=True)
+parser.add_argument(
+    "-w",
+    "--web-root",
+    help="Путь расположения файлов на исходном сервере",
+    required=True,
+)
+parser.add_argument(
+    "-p",
+    metavar="PORT",
+    help="Задать порт ssh в случае если на исходном сервере кастомный порт",
+)
+parser.add_argument(
+    "--pas",
+    metavar="PASSWORD",
+    help="Если нельзя прокинуть ключ, то можно для автоматизаци задать пароль",
+)
+parser.add_argument("--fix-permission", help="Исправление прав")
+parser.add_argument(
+    "--php", help="Задать требуемую версию php при разворачивании автоматически"
+)
+parser.add_argument(
+    "--log-file",
+    help="Кастомный путь до лог файла, удобнее когда много сайтов переносится",
+)
+parser.add_argument("--help", action="help")
 
 
 args = parser.parse_args()
 
 log = logger.setup_logger()
 
-log.info('test start')
+log.info("test start")
+
 
 def main():
-    log.warning('test main')
+    log.warning("test main")
     pass
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
